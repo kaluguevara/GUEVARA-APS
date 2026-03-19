7dc1cd572ac41203fdba828cd7336d879dbe9606
kaluguevara <guevarakala@gmail.com>
2026-03-19 17:33:02 -0300

    Subo mi version 2.0

--- TS0/ts0.py
+++ TS0/ts0.py
@@ -8,21 +8,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-# ===================================
+#%%
 #Ejecicio TP0
 def funcion_seno(amp, desplazo, frec, fase, cant_muestras, frec_muestreo):
     # Crear el vector de tiempos
     tt = (np.arange(cant_muestras)/frec_muestreo).reshape(-1, 1)
+    
     # Calcular la señal senoidal
     xx = (amp * np.sin(2 * np.pi * frec * tt + fase) + desplazo).reshape(-1, 1)
     return tt, xx
-"""
-reshape asegura que los vectores tt y xx tengan forma (Nx1)
-que es lo que pide la consigna. Tambien se puede hacer con np.linspace. 
-Segun el diccionario de numpy, el -1 calcula el tamaño de la dimension, 
-en este caso, nro de muestras. Y el 1, fija la segunda dimension en 1, 
-o sea N filas x 1 columna.
-"""
+    
     # Generar señal
 tt, xx = funcion_seno(amp=1, desplazo=0.5, frec=5, fase=np.pi/4, cant_muestras=1000, frec_muestreo=1000)
     
@@ -35,33 +30,38 @@ plt.ylabel('Amplitud (V)')
 plt.grid(True)
 plt.show()
 
-# ==========================================
-#Bonus: Experimento con frecuencias vistas en clase
+#%%
+#Bonus: Experimento con frecuencias vistas en clase:
 lista_frec_experimento = [500,999,1001,2001]
 plt.figure(2)
 for i in range(len(lista_frec_experimento)):
     frec = lista_frec_experimento[i]
-    tt, xx = funcion_seno(amp=1, desplazo=0.5, frec=frec, fase=np.pi/4, cant_muestras=1000, frec_muestreo=1000)
-    plt.subplot(2, 2, i+1)  
-    #plt.subplot(2 filas, 2 columnas, 1 a 4 plots.
-    #al i le sumo 1 porque el subplot siempre arranca en 1
-    plt.plot(tt, xx)
-    plt.title(f'Funcion seno con frecuencia: {frec} Hz')
-    plt.xlabel('Tiempo (s)')
-    plt.ylabel('Amplitud (V)')
-    plt.grid(True)
+    if (frec == 500):
+        tt, xx = funcion_seno(amp=1, desplazo=0.5, frec=frec, fase=np.pi/4, cant_muestras=1000, frec_muestreo=25000)
+        plt.subplot(2, 2, i+1)  
+        plt.plot(tt, xx)
+        plt.title(f'Funcion seno con frecuencia:{frec}Hz')
+        plt.xlabel('Tiempo (s)')
+        plt.ylabel('Amplitud (V)')
+        plt.grid(True)
+    else:
+        tt, xx = funcion_seno(amp=1, desplazo=0.5, frec=frec, fase=np.pi/4, cant_muestras=1000, frec_muestreo=1000)
+        plt.subplot(2, 2, i+1)  
+        plt.plot(tt, xx)
+        plt.title(f'Funcion seno con frecuencia:{frec}Hz')
+        plt.xlabel('Tiempo (s)')
+        plt.ylabel('Amplitud (V)')
+        plt.grid(True)
 
 plt.tight_layout()
-#ajusta automáticamente los espacios entre subplots para que no haya solapamiento o cortes,
-#basicamente para que no quede feo
 plt.show()
 
-# ==========================================
-#Bonus: Experimentar con otra funcion, elegi sawtooth, solo cambia que 
-#agrego un argumento que es el ancho de los dientes de sierra en la funcion sawtooth
+#%%
+#Bonus: Experimentar con otra funcion:
 def generar_sawtooth(amp, desplazo, frec, fase, cant_muestras, frec_muestreo, width):
     # Vector de tiempos con arange
     tt = (np.arange(cant_muestras)/frec_muestreo).reshape(-1, 1)
+    
     # Generar señal sawtooth
     xx = (amp * signal.sawtooth(2 * np.pi * frec * tt + fase, width=width) + desplazo).reshape(-1, 1)
     return tt, xx
@@ -76,4 +76,70 @@ plt.title('Funcion sawtooth (diente de sierra)')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Amplitud (V)')
 plt.grid(True)
+plt.show()
+
+#%%
+#potencia de señal
+vmax=np.sqrt(2)
+tt, xx = funcion_seno(vmax, 0.5, 5, np.pi/4, 1000, 1000)
+Px = np.var(xx)
+
+SNR = 20
+
+Pr = 1/(10**(SNR/10)) #en clase lo puse disitno estaba mal
+
+#agrego esto porque sino me tira error, falta variables en random normal
+mu = 0
+n = len(xx)  
+
+#el ruido
+U_n = np.random.normal(mu, np.sqrt(Pr), n).reshape(-1,1)
+#le puse reshape porque me tiraba error con las dimensiones
+
+#señal mas ruido
+xxn = xx + U_n
+
+#tuqui grafico
+plt.figure(4)
+plt.clf()
+plt.plot(tt, xxn, label='Señal con ruido')
+plt.plot(tt, xx, 'r', lw=2, label='Señal original')
+plt.title(f'f0 = {frec} Hz')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+#%%
+from scipy import signal as sig
+n0 = 300   #muestras
+
+dd = np.zeros(n0+1)
+dd[n0] = 1
+
+yy = sig.convolve(xx.flatten(), dd) #tmb me tiraba error con dimensiones
+
+plt.figure(5)
+plt.clf()
+plt.plot(yy)
+
+
+#%%
+yy = 1/n * sig.convolve(U_n, np.flip(U_n))
+
+plt.figure(6)
+plt.clf()
+plt.plot(yy)
+
+
+#%%
+B = 3  # bits
+Vfs = 3  # Volts
+
+qq = Vfs / 2**B
+xxq = np.round(xx/qq)
+#el error es e=xx-xxq, pero deberia multiplicar por qq para pasar de los niveles a volts devuelta
+ee = (xx-xxq)*qq
+plt.figure(7)
+plt.hist(ee)
+plt.title('error de quant.')
 plt.show()
